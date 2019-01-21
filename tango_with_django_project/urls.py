c6bcb8f6e638eb8844336a987dbde1bd67e2538c
maxine-jay <maxinejaysimpson@gmail.com>
2019-01-21 14:42:34 +0000

    end of chapter 3.5

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from rango import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
+    #above maps any URL starting
+    #with rango/ to be handled by
+    #the rango application
     url(r'^admin/', admin.site.urls),
-]
+    ]
