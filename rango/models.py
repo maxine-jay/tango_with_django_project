d51641473ebb638f45d2e91acec4a8909b9e837e
maxine-jay <maxinejaysimpson@gmail.com>
2019-01-29 18:28:36 +0000

    Almost at the end of Chapter 5 - need to customise the admin interface

--- rango/models.py
+++ rango/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
 	
+	class Meta:
+		verbose_name_plural = 'Categories'
+		
 	def __str__(self): # For Python 2, use __unicode__ too
 		return self.name
 		
