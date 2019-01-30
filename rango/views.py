97f4ed5a3c4222361aeaa53de221a0262bb58d9e
maxine-jay <maxinejaysimpson@gmail.com>
2019-01-30 16:14:15 +0000

    modified index template

--- rango/views.py
+++ rango/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
     #construct a dictionary to pass to the template engine as its context
     # note the key bold message is the same as {{boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    
-    #return a rendered response to send to the client
-    #we make use of the shortcut function to make our lives easier
-    #note that the first parameter is the template we wish to use
-    return render(request, 'rango/index.html', context=context_dict)
-
+    # context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # this queries category model to retrieve top 5 categories.
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
+	return render(request, 'rango/index.html', context=context_dict)
+	
 def about(request):
 	context_dict = {'boldmessage': "This tutorial was created by Maxine"}
 	return render(request, 'rango/about.html', context=context_dict)
