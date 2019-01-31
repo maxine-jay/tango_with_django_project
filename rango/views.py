3ff1c0c1fda963ee59c346ba785bd319fae7e327
maxine-jay <maxinejaysimpson@gmail.com>
2019-01-31 20:02:34 +0000

    added category links on index page

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     #construct a dictionary to pass to the template engine as its context
@@ -14,3 +15,20 @@ def index(request):
 def about(request):
 	context_dict = {'boldmessage': "This tutorial was created by Maxine"}
 	return render(request, 'rango/about.html', context=context_dict)
+	
+def show_category(request, category_name_slug):
+	context_dict = {}
+	
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		
+		pages = Page.objects.filter(category=category)
+		
+		context_dict['pages'] = pages
+		
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+	
+	return render(request, 'rango/category.html', context_dict)
