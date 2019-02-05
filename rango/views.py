4fc33ca0090f0adecf72aed72b820b2622df2e7a
maxine-jay <maxinejaysimpson@gmail.com>
2019-02-05 17:53:35 +0000

    almost finished Chapter 7- some errors with add_page

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def index(request):
     #construct a dictionary to pass to the template engine as its context
@@ -37,3 +39,46 @@ def show_category(request, category_name_slug):
 		context_dict['pages'] = None
 	
 	return render(request, 'rango/category.html', context_dict)
+	
+def add_category(request):
+	form = CategoryForm()
+	
+	#A HTTP POST?
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		
+		#Have we been provided with a valid form?
+		if form.is_valid():
+			form.save(commit=True)
+			#now that the category is saved
+			#we could give a confirmation message
+			#but since the most recent category added is on the index page_list
+			#then we can direct the user back to the index page_list
+			return index(request)
+		else:
+			# The supplied form contained errors -
+			# just print them to the terminal.
+			print(form.errors)
+	return render(request, 'rango/add_category.html', {'form' : form})
+	
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+		
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+	
+	context_dict = {'form':form, 'category': category}
+	return render(request, 'rango/add_page.html', context_dict)
